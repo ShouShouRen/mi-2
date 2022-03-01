91d36ce4c3f42075f2c8c13deeac42c136f32a9d
Peter <peterchen2017125@gmail.com>
2022-03-01 13:23:25 +0800

    01

--- bulma/index.js
+++ bulma/index.js
@@ -25,12 +25,19 @@ tabs.forEach((tab) => {
         })
     })
 })
+// modal
+
+const signupButton  = document.querySelector('#signup');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
 
 
+signupButton.addEventListener('click', () => {
+    modal.classList.add('is-active');
+})
 
-
-
-
-
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+})
 
 
